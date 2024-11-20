4573b22c82ce0a89d4ad2448b7cbb82b31f17e89
Laury <112404773+Belywen@users.noreply.github.com>
2024-11-20 12:02:23 +0100

    maj

--- todolist/js/script.js
+++ todolist/js/script.js
@@ -1,14 +1,29 @@
-const addBtn = document.querySelector('#btn');
-addBtn.addEventListener('click', addTask);
+//document > document html
+//query selector > selecteur (css)
+//addEventListener > Ecoute ce qu'il se passe (clic de souris)
+//cloneNode > noeud à dupliquer (vrai : enfants doivent être clonés)
 
 
-const taskCard = document.querySelector(".todoCard");
-const tasksContainer = document.querySelector('#todoCards');
+const addBtn = document.querySelector('#btn'); //Récupére le bouton d'ajout 
+const taskCard = document.querySelector(".todoCard"); //Récupére la class de la carte
+const tasksContainer = document.querySelector('#todoCards'); //Récupérer le container de la class
+const delBtn = document.querySelector('.delBtn'); // Récupére la poubelle
+const compteur = document.querySelector('.compteur'); //Récupere le compteur
 
-//Ajout d'une card
+addBtn.addEventListener('click', addTask); //Ecoute bouton
+
+
+//Ajout d'une card : 
+//qui va duppliquer la carte complete
+//qui va declarer une nouvelle variable pour toute nouvelle icone
+//qui va déclarer une nouvelle variable pour tout nouvel textarea
+//qui va afficilier une valeur au textarea
+//qui appelle la fonction delete lors de l'écoute du clic
+//qui demande d ajouter les enfants de totoCards et donc de les duppliquer
+
 function addTask() {
-    const newTask = taskCard.cloneNode(true);
-    const newDelBtn = newTask.querySelector('.delBtn');
+    const newTask = taskCard.cloneNode(true); 
+    const newDelBtn = newTask.querySelector('.delBtn'); 
     const newTextArea = newTask.querySelector('.task');
 
     newTextArea.value = "New task";
@@ -20,23 +35,24 @@ function addTask() {
     updateCount();
 }
 
-//Suppression d'une card
-const delBtn = document.querySelector('delBtn');
+//Suppression d'une card :
+//Appelle de la fonction delete quand la poubelle est cliquée
 delBtn.addEventListener('click', function() {
     deleteTask(taskCard)
 });
 
+//Fonction qui supprime une card
 function deleteTask(task) {
     task.remove();
+    updateCount();
 }
 
 //Fonction compteur
-
-function count() {
-    while(int i > 0) {
-        i++;
-    }
+function updateCount(){
+    const taskCard = tasksContainer.children.length; //Récupére nombre de cartes situés dans le container (tableau)
+    compteur.textContent = taskCard ; //compte le nombre de cartes
 }
+
 
 
 
