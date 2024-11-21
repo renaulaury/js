2510433fdb8c3abae4a09235934fdc5e95877579
Lily <112404773+Belywen@users.noreply.github.com>
2024-11-21 11:46:48 +0100

    maj exo2

--- base-carres/exo2/script.js
+++ base-carres/exo2/script.js
@@ -1,21 +1,33 @@
 const square = document.createElement('div'); //Crée une div
 square.classList.add("carre"); //Ajoute une class a la div
 
-const squareLead = document.createElement('div');
-squareLead.classList.add("squareLead");
-
+const colors = ["green", "blue", "yellow", "red"];
 
 function addSquares() {   
-   for(let i = 1; i <= 4; i++) {    
-    const newSquare = square.cloneNode(); //Clone le carre
-    wrapperGroup.appendChild(newSquare); //Boucles les carrés dans le container
-    }    
+    for(let i = 0; i < 4; i++) {    
+     const newSquare = square.cloneNode(); //Clone le carre
+     wrapperGroup.appendChild(newSquare); //Boucles les carrés dans le container
+     newSquare.style.backgroundColor = colors[i];
+
+     changeColor(newSquare);
+    }         
+ }
+ 
+ //Création et mep du carré leader
+ const squareLead = document.createElement('div');
+ squareLead.classList.add("squareLead");
+ wrapper.appendChild(squareLead);
+ 
+ 
+ addSquares();
+
+function changeColor(newSquare) {
+    newSquare.addEventListener('click', function(){
+        squareLead.style.backgroundColor = newSquare.style.backgroundColor;
+     })
 }
 
-const newSquareLead = square.cloneNode();
-wrapper.appendChild(newSquareLead);
-
-
-addSquares();
-
+ //Quand je clique sur un carré alors le leader prend sa couleur
+ //j ai besoin addeventlist sur chaque carre
+ //
 
